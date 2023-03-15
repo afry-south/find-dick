a9afb935092e1884baae9a15a8b50b3a89aa992b
Heimer <ronnie.heimer@afconsult.com>
2023-03-15 16:37:43 +0100

    sound fixes

--- sound.py
+++ sound.py
@@ -1,4 +1,5 @@
 from pygame import mixer
+import os
 import random
 from settings import bomb_sound, found_sound, game_over_sound, sound_file_extension, sound_dir
 from os.path import exists
@@ -10,19 +11,26 @@ class Sound :
 
 
     def play(self, song) :
+    
         print("SONG: " + song)
-        path_start = sound_dir + "/" + str(song).lower() + "." + sound_file_extension
-        print(path_start)
-
-        if "hjärter" in song or "spader" in song or "klöver" in song or "ruter" in song or (not exists(path_start)):
-            print("HELLO")
+        if "hjärter" in song or "spader" in song or "klöver" in song or "ruter" in song :
             song = "default"
-
-        path = sound_dir + "/" + str(song).lower()
-
+            
         if song == "default" or song == "Dick" or song == "bomb" :
-            path += str(random.randint(0,8))
-        path += "." + sound_file_extension
+            path = sound_dir + "/" + str(song).lower() + str(random.randint(0,8)) + "." + sound_file_extension
+        
+        else :
+        
+            list = os.listdir(sound_dir)
+            print(list)
+            print(song + "0." + sound_file_extension)
+            tmp = song + "0." + sound_file_extension
+            if tmp.lower() not in list :
+                path = sound_dir + "/" + "default" + str(random.randint(0,8)) + "." + sound_file_extension
+            else :
+        
+                path = sound_dir + "/" + str(song).lower() + "0." + sound_file_extension
+            
         print("PATH: " + path)
         mixer.music.load(path)
         mixer.music.play()
